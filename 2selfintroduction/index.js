b812b8aa59f5de29e6bf7c7f5e7c51e9f2af2f65
NaGi77 <245479702@qq.com>
2016-04-02 23:15:11 +0800

    create selfintroductin

---
+++ 2selfintroduction/index.js
@@ -0,0 +1,41 @@
+var winWidth1;
+var winHeight;
+var boxwidth1;
+var boxheight1;
+function a(){
+	if (window.innerWidth)
+		winWidth1 = window.innerWidth;
+	else if ((document.body) && (document.body.clientWidth))
+		winWidth1 = document.body.clientWidth;
+	if(winWidth1<=1200)
+		boxwidth1=document.getElementById("box").style.width="1200px";
+	else if(winWidth1>=1200)
+		boxwidth1=document.getElementById("box").style.width="100%";
+	if (window.innerHeight)
+		winHeight = window.innerHeight;
+	else if ((document.body) && (document.body.clientHeight))
+		winHeight = document.body.clientHeight;
+	if(winHeight<=600)
+		document.getElementById("box").style.height="1200px";
+	else if(winHeight>=600)
+		document.getElementById("box").style.height="150%";
+}
+function b(){
+	if(boxwidth1<=1200)
+		document.getElementById("top").style.width="1200px";
+	else if(boxwidth1>=1200)
+		document.getElementById("top").style.width="100%";
+	if(boxheight1<=600)
+		document.getElementById("top").style.height="25%";
+	else if (boxheight1>=600) 
+		document.getElementById("top").style.height="20%";
+
+}
+window.onload=function(){
+	a();
+	b();
+}
+window.onresize=function(){
+	a();
+	b();
+}
